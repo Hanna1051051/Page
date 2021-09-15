e3ceef963b7f260821b37a8c95b2812be1d21341
Hanna <1051051@mail.ru>
2021-09-15 23:05:14 +0300

    add Homework_14

--- src/main/java/pageobject/mail/MailPage.java
+++ src/main/java/pageobject/mail/MailPage.java
@@ -1,7 +1,7 @@
 package pageobject.mail;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
